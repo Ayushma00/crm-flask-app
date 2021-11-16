32e6c9ce50d7d7628018e1d27a2b7ee220ff5ef8
Ayushma00 <pantaayushma77@gmail.com>
2021-11-16 21:51:24 +0545

    Create new values in db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,request,redirect
+# call post request methodS
 import config
 import requests
+# requests library for making differenyt api calls
 import json
 def page_not_found(e):
   return render_template('404.html'), 404
@@ -16,12 +18,30 @@ headers={'authorization':'Bearer {}'.format(app.config['AIRTABLE_KEY']),'content
 def index():
     return render_template('index.html', **locals())
 
-@app.route('/users')
+@app.route('/users',methods=['GET','POST'])
 def users():
-    url='https://api.airtable.com/v0/appZ8wloM4omloj0T/Team?maxRecords=4&view=Grid%20view'
+    url='https://api.airtable.com/v0/appZ8wloM4omloj0T/Team?maxRecords=10&view=Grid%20view'
     r=requests.get(url,headers=headers)
     result=json.loads(r.text)
     print(result)
+    if request.method=='POST':
+        payload={
+        "records": [
+                      {
+                          "fields": {
+
+                            "teamid": request.form['teamId'],
+                            "FirstName": request.form['FirstName'],
+                            "LastName": request.form['LastName'],
+                            "JobTitle": request.form["JobTitle"],
+                            "Email": request.form["Email"],
+                            "PhoneNumber": request.form['PhoneNumber']
+                            }
+                            }
+                    ]
+                    }
+        requests.post('https://api.airtable.com/v0/appZ8wloM4omloj0T/Team',json=payload,headers=headers)
+        return redirect(url_for('users'))
     return render_template('user.html',**locals())
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
